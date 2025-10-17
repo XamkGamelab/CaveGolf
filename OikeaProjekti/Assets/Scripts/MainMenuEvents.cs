26a3b3afe036bd15331a6eb6348c9056f08125c9
JennaV001 <gjevi010@edu.xamk.fi>
2025-10-17 12:21:13 +0300

    PlayButton script

--- OikeaProjekti/Assets/Scripts/MainMenuEvents.cs
+++ OikeaProjekti/Assets/Scripts/MainMenuEvents.cs
@@ -1,10 +1,21 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UIElements;
 
 public class MainMenuEvents : MonoBehaviour
 {
-    public void NextScene()
+    [SerializeField] UIDocument uiDocument;
+    private VisualElement root;
+    void Start()
     {
+        root = uiDocument.rootVisualElement;
+
+        var playButton = root.Q<VisualElement>("PlayButton");
+
+        playButton.RegisterCallback<ClickEvent>(OnClickEvent);
+    }
+
+    public void OnClickEvent(ClickEvent evt) { 
         SceneManager.LoadScene("Level1");
     }
 }

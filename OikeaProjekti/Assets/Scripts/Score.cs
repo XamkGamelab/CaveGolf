ba599e6e7b7a4be1a3d8c95b40962e6e7f3bf330
mikser <miika.puonti@gmail.com>
2025-11-07 00:29:59 +0200

    SCORE: classes-combined

--- OikeaProjekti/Assets/Scripts/Score.cs
+++ OikeaProjekti/Assets/Scripts/Score.cs
@@ -1,28 +1,28 @@
+using TMPro;
 using UnityEngine;
 
-public class Score
+public class Score : MonoBehaviour
 {
+
     public static int Local { get; private set; }
     public static int Total { get; private set; }
-    public static ScoreUI CurrentInstance;
+
+
 
     public static void Add(int i)
     {
         Local += i;
-        if (CurrentInstance != null) 
-            CurrentInstance.UI_UPADTE();
+        if (Instance != null)
+            Instance.UI_UPADTE();
     }
 
     public static void UpdateTotal()
     {
         Total += Local;
         Local = 0;
-        if (CurrentInstance != null)
-            CurrentInstance.UI_UPADTE();
+        if (Instance != null)
+            Instance.UI_UPADTE();
     }
-
-    
-
     public static void Save()
     {
         Debug.LogError("ERROR: SCORE SAVING IS NOT IMPLEMENTED YET");
@@ -30,5 +30,25 @@ public class Score
     public static void Load()
     {
         Debug.LogError("ERROR: SCORE LOADING IS NOT IMPLEMENTED YET");
+    }
+
+
+
+    // things used by instances of script, should not be accessed elsewhere
+
+    [HideInInspector]
+    static Score Instance;
+    [HideInInspector]
+    TextMeshProUGUI text;
+    void UI_UPADTE()
+    {
+        text.text = " " + Total.ToString() + (Local != 0 ? ("<br>+" + Local.ToString()) : "");
+    }
+    void Start()
+    {
+        text = GetComponent<TextMeshProUGUI>();
+        UI_UPADTE();
+        if (Instance != null) Debug.LogWarning("Replacing existing Score instance with new one, there may be multiple in scene?");
+        Instance = this;
     }
 }

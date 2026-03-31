8a0d909f7c5f25ed534eed63dd86bc98f2e876e7
taina <nope.com>
2026-03-31 14:54:13 +0300

    SingletonMono : Fixed warning UIUserManager: Moved to the correct folder

--- OikeaProjekti/Assets/Scripts/Utilities/SingletonMono.cs
+++ OikeaProjekti/Assets/Scripts/Utilities/SingletonMono.cs
@@ -29,9 +29,9 @@ public class SingletonMono<T> : MonoBehaviour where T : MonoBehaviour
             {
                 if (_instance == null)
                 {
-                    _instance = (T)FindObjectOfType(typeof(T));
+                    _instance = (T)FindAnyObjectByType(typeof(T));
 
-                    if (FindObjectsOfType(typeof(T)).Length > 1)
+                    if (FindObjectsByType(typeof(T),FindObjectsSortMode.None).Length > 1)
                     {
                         Debug.LogError("[Singleton] Something went really wrong " +
                             " - there should never be more than 1 singleton!" +
